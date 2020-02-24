31a0bdfe3ff3ec6c04041bf7cf4e27ee97f4da49
oskarivilen <54893168+oskarivilen@users.noreply.github.com>
2020-02-24 17:34:10 +0200

    Add files via upload

---
+++ ShoeStamina/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Listing from "./Listing";
+  
+var destination = document.querySelector("#container");
+
+  
+ReactDOM.render(
+    <div>
+     
+        <Listing/>
+    </div>,
+    destination
+);
