c59aee2f91d0fd8f7cfb10a4ece326be306cb963
oskarivilen <54893168+oskarivilen@users.noreply.github.com>
2020-03-04 14:46:33 +0200

    Update Listing.js

--- ShoeStamina/src/Listing.js
+++ ShoeStamina/src/Listing.js
@@ -2,6 +2,62 @@ import React, { Component } from "react";
 import ShoeItems from "./ShoeItems";
 import "./Listing.css";
 
+class ProgressBarExample extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      percentage: 0 
+    }
+    
+    this.nextStep = this.nextStep.bind(this)
+  }
+  
+  nextStep() {
+    if(this.state.percentage === 100) return 
+    this.setState(prevState => ({ percentage: prevState.percentage + 5 }))
+  }
+  
+  render() {
+    return (
+      <div>
+        
+        <h2> Kilometers done </h2>
+        <ProgressBar percentage={this.state.percentage}  />
+        
+        <div style={{ marginTop: '20px' }}>  
+          <button 
+            onClick={this.nextStep}
+           >
+            Log run
+          </button>  
+        </div>   
+        
+        <div style={{marginTop: '10px', color: 'blue', marginBottom: '15px'}} onClick={() => this.setState({ percentage: 0 })}>
+          Reset
+        </div>
+      </div>
+    )
+  }  
+}
+
+const ProgressBar = (props) => {
+  return (
+      <div className="progress-bar">
+        
+        <Filler percentage={props.percentage}/>
+        
+      </div>
+    )
+}
+
+const Filler = (props) => {
+  return <div className="filler" style={{ width: `${props.percentage}%` }} />
+}
+
+
+
+
 class Listing extends Component {
 
   state = {
@@ -33,6 +89,7 @@ class Listing extends Component {
     });
   }
   
+  
   render() {
     return (
       <div className="shoelistMain">
@@ -41,9 +98,12 @@ class Listing extends Component {
           <input id="km" placeholder="Kilometers run" value={this.state.km}/>
           <input id="shoes" placeholder="Shoes" value={this.state.shoes}/>
           <input id="link" placeholder="Link to shoes" value={this.state.link}/>
+          
           <button onClick={this.addItem}>add</button>
         </div>
+    
         <ShoeItems entries={this.state.items}/>
+        <ProgressBarExample />
       </div>
     );
   }
