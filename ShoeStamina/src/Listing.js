c42803a436a3d05d4866d4b4cb44aa79bd4b12d9
oskarivilen <54893168+oskarivilen@users.noreply.github.com>
2020-03-04 15:03:18 +0200

    Update Listing.js

--- ShoeStamina/src/Listing.js
+++ ShoeStamina/src/Listing.js
@@ -2,60 +2,6 @@ import React, { Component } from "react";
 import ShoeItems from "./ShoeItems";
 import "./Listing.css";
 
-class ProgressBarExample extends React.Component {
-  constructor(props) {
-    super(props)
-    
-    this.state = {
-      percentage: 0 
-    }
-    
-    this.nextStep = this.nextStep.bind(this)
-  }
-  
-  nextStep() {
-    if(this.state.percentage === 100) return 
-    this.setState(prevState => ({ percentage: prevState.percentage + 5 }))
-  }
-  
-  render() {
-    return (
-      <div>
-        
-        <h2> Kilometers done </h2>
-        <ProgressBar percentage={this.state.percentage}  />
-        
-        <div style={{ marginTop: '20px' }}>  
-          <button 
-            onClick={this.nextStep}
-           >
-            Log run
-          </button>  
-        </div>   
-        
-        <div style={{marginTop: '10px', color: 'blue', marginBottom: '15px'}} onClick={() => this.setState({ percentage: 0 })}>
-          Reset
-        </div>
-      </div>
-    )
-  }  
-}
-
-const ProgressBar = (props) => {
-  return (
-      <div className="progress-bar">
-        
-        <Filler percentage={props.percentage}/>
-        
-      </div>
-    )
-}
-
-const Filler = (props) => {
-  return <div className="filler" style={{ width: `${props.percentage}%` }} />
-}
-
-
 
 
 class Listing extends Component {
@@ -103,7 +49,7 @@ class Listing extends Component {
         </div>
     
         <ShoeItems entries={this.state.items}/>
-        <ProgressBarExample />
+        
       </div>
     );
   }
