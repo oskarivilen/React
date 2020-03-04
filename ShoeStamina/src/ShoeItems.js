45a52623949759066dda27beb025439b52abede2
oskarivilen <54893168+oskarivilen@users.noreply.github.com>
2020-03-04 15:04:03 +0200

    Update ShoeItems.js

--- ShoeStamina/src/ShoeItems.js
+++ ShoeStamina/src/ShoeItems.js
@@ -1,22 +1,77 @@
 import React, { Component } from "react";
 
 
+class ProgressBarExample extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      percentage: 0 
+    }
+    
+    this.nextStep = this.nextStep.bind(this)
+  }
+  
+  nextStep() {
+    if(this.state.percentage === 100) return 
+    this.setState(prevState => ({ percentage: prevState.percentage + 5 }))
+  }
+  
+  render() {
+    return (
+      <div>
+        
+        <h2> Kilometers done </h2>
+        <ProgressBar percentage={this.state.percentage}  />
+        
+        <div style={{ marginTop: '20px' }}>  
+          <button 
+            onClick={this.nextStep}
+           >
+            Log run
+          </button>  
+        </div>   
+        
+        <div style={{marginTop: '10px', color: 'blue', marginBottom: '15px'}} onClick={() => this.setState({ percentage: 0 })}>
+          Reset
+        </div>
+      </div>
+    )
+  }  
+}
+
+const ProgressBar = (props) => {
+  return (
+      <div className="progress-bar">
+        
+        <Filler percentage={props.percentage}/>
+        
+      </div>
+    )
+}
+
+const Filler = (props) => {
+  return <div className="filler" style={{ width: `${props.percentage}%` }} />
+}
+
 
 class ShoeItems extends Component {
   render() {
-    var todoEntries = this.props.entries;
+    var shoeEntries = this.props.entries;
     var listItems = [];
 
-    for (var i = 0; i < todoEntries.length; i++) {
-      var entry = todoEntries[i];
+    for (var i = 0; i < shoeEntries.length; i++) {
+      var entry = shoeEntries[i];
       listItems.push(<li key={"item" + i}>
-        <a href={entry.link}>{entry.shoes}</a> Kilometers run: {entry.km}
+        <a href={entry.link}>{entry.shoes}</a> || Kilometers run: {entry.km}
+        <ProgressBarExample />
       </li>);
     }
 
     return (
       <ul className="theList">
         {listItems}
+        
       </ul>
     );
   }
